20b505c91230fffe83dd40341e03c4ec57739d19
Himanshu <aggarwakh87@gmail.com>
2025-12-20 16:50:45 +0530

    Error throwing

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,13 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import adapter_manager
-import app_settings
 import flutter_blue_plus_darwin
 import geolocator_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AdapterManagerPlugin.register(with: registry.registrar(forPlugin: "AdapterManagerPlugin"))
-  AppSettingsPlugin.register(with: registry.registrar(forPlugin: "AppSettingsPlugin"))
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
 }
