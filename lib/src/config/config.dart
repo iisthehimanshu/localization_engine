3788d25c9de8396a810cf727b972e5beb8797a3a
Himanshu <aggarwakh87@gmail.com>
2026-02-25 15:23:59 +0530

    beacon battery status added

--- lib/src/config/config.dart
+++ lib/src/config/config.dart
@@ -14,7 +14,7 @@ class AppConfig {
     if (kDebugMode) {
       return 'https://dev.iwayplus.in';
     } else {
-      return 'https://dev.iwayplus.in';
+      return 'https://maps.iwayplus.in';
     }
   }
 
